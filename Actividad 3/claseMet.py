0897aa593fc565bc346be2e034d1f0650102bf69
facuDev <ariasfacu1998@gmail.com>
2023-04-29 17:13:31 -0300

    Trabajado hasta el punto 1 de la actividad 3

--- Actividad 3/claseMet.py
+++ Actividad 3/claseMet.py
@@ -4,6 +4,7 @@ Created on Tue Apr 25 18:45:24 2023
 
 @author: Facu
 """
+import csv
 
 class Registro:
     __dia=0
@@ -13,17 +14,46 @@ class Registro:
     __varPsAtm=0
     
     def __init__(self,temp,hum,psatm,dia,hora):
-        self.__dia = dia
-        self.__hora = hora
         self.__varTemp = temp
         self.__varHum = hum
         self.__varPsAtm = psatm
+        self.__dia = dia
+        self.__hora = hora
+
+    def __repr__(self):
+        return f'| Dia | Hora |Temperatura| Humedad | Presion Atm |\n    {self.__dia}     {self.__hora}     {self.__varTemp}     {self.__varHum}         {self.__varPsAtm}' 
+
         
 class Manejador:
-    def __init__(self):
-        lista[][]
+    def __init__(self,):
+       self.__regMet = [[] for _ in range(2)]
+
+
+    def addReg(self, reg, dia):
+       self.__regMet[dia-1].append(reg)
+
+    def testRegMet(self):
+        archi = open('registro.csv')
+        reader = csv.reader(archi, delimiter=";")
+
+        for fila in reader:
+            dia = int(fila[0]) 
+            hora = int(fila[1])
+            temp = float(fila[2])      
+            humedad = float(fila[3])
+            presion = float(fila[4])
+            reg = Registro(temp, humedad, presion, dia, hora)
+            self.addReg(reg, dia)
+        archi.close()
         
         
+    def __str__(self):
+         
+        s = " "
+        for dia in range(len(self.__regMet)):
+            for fila in self.__regMet[dia]:
+                s += str(fila) + '\n'
+        return s
         
 
     
--- Actividad 3/programa.py
+++ Actividad 3/programa.py
@@ -5,12 +5,14 @@ Created on Tue Apr 25 18:53:28 2023
 @author: Facu
 """
 from claseMet import Registro
-
-import datetime
+from claseMet import Manejador
 import csv
 
 if __name__ == '__main__':
     
+    mr = Manejador()
+    mr.testRegMet()
+    print(mr)
     
     
 
