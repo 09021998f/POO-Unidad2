6e721e906760cc5aa191f23df56275efd035b607
facuDev <ariasfacu1998@gmail.com>
2023-05-06 17:44:50 -0300

    Borrando basura

--- Actividad 1/programa.py
+++ Actividad 1/programa.py
@@ -3,24 +3,22 @@ from claseEmail import ManejadorEmail
 import csv
 import os
 if __name__ == '__main__':
-    #objeto = Email("facucabj","google","com",1234)
-    """
+    objeto = Email("facucabj","google","com",1234)
+    
     nombre = input("Nombre:")
     correo = input("Correo: ")
     objeto.crearCuenta(correo)
-    """
-    """
+    
+    
     print("**Cuenta**")
     print(objeto)
     print("*"*10)
-#   objeto.getEmail(correo, nombre)
+    objeto.getEmail(correo, nombre)
     cont=int(input("Contraseña: "))
     objeto.cambioContraseña(cont)
-    """
-    #cwd = os.getcwd()  # Get the current working directory (cwd)
-    #files = os.listdir(cwd)  # Get all the files in that directory
-    #print("Files in %r: %s" % (cwd, files)) 
     
+ 
+ 
     mE = ManejadorEmail()
     mE.testEmails()
     print(mE)
--- Actividad 2/ejercicio6.py
+++
@@ -1,8 +0,0 @@
-from claseViajeroFrecuente import Manejador
-from claseViajeroFrecuente import ViajeroFrecuente
-
-if __name__ == '__main__':
-    mv = Manejador()
-    mv.testViajero()
-    print(mv.viajeroConMayorCantidadDeMillas())
-    
